5c54ff29183a3afd8a14f409053455d28e8d157e
vedanth dasari <vd908@nyu.edu>
2019-10-16 16:56:13 -0400

    Worked on Flake

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+
+
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'swe1app.settings')
     try:
@@ -15,5 +17,7 @@ def main():
             ) from exc
 
     execute_from_command_line(sys.argv)
+
+
 if __name__ == '__main__':
     main()
